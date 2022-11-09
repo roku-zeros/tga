5d2ee8967009ee8b9194312a14c601228e3ec76c
Zerogi <rob.zgonnikov@gmail.com>
2022-11-09 07:30:32 +0300

    - fix

---
+++ passenger_wsgi.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import os, sys
+sys.path.insert(0, '/var/www/u1813889/data/www/wolf-mag.ru/tga')
+sys.path.insert(1, '/var/www/u1813889/data/djangoenv/lib/python3.7/site-packages')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'project_name.settings'
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
